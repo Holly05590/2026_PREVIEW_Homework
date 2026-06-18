14dcb06363af8406a97057235bae773196e4291d
Holly05590 <ilexholly@gmail.com>
2026-06-19 06:11:26 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,120 @@
+const http= require('http');
+const { v4: uuidv4 } = require('uuid');
+const errorHandle = require('./errorHandle');
+const todos = [];
+const requestListener = function (req, res) {
+    const headers = {
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+        'Content-Type': 'application/json'};
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk;
+    });
+        
+    if (req.url == '/todos' && req.method == 'GET') {
+    res.writeHead(200,headers);
+    res.write(JSON.stringify({
+        "status": "success",
+        "data": todos,
+        }));
+    res.end();
+    } else if (req.url == '/todos' && req.method == 'POST') {
+        req.on('end', () => {
+            try{
+                const title = JSON.parse(body).title;
+                if (title !== undefined) {
+                    const todo = {
+                    "title": title,
+                    "id": uuidv4(),
+                }
+                todos.push(todo);
+                res.writeHead(200,headers);
+                res.write(JSON.stringify({
+                "status": "success",
+                "data": todos,
+                }));
+                res.end();
+                } else {
+                    errorHandle(res);
+                }
+                
+            }catch(error){
+                errorHandle(res);
+            }
+        });
+    } else if (req.url == '/todos' && req.method == 'DELETE') {
+        todos.length = 0;
+        res.writeHead(200,headers);
+        res.write(JSON.stringify({
+            "status": "success",
+            "data": todos,
+            "message": "清空成功",
+            }));
+            res.end();
+    } else if (req.url.startsWith('/todos/') && req.method == 'DELETE') {
+        const id = req.url.split('/').pop();
+        const index = todos.findIndex(todo => todo.id === id); 
+        if (index !== -1) {
+            todos.splice(index, 1);
+            res.writeHead(200,headers);
+            res.write(JSON.stringify({
+                "status": "success",
+                "data": todos,
+                "message": "刪除成功",
+                }));
+                res.end();
+        } else {
+            res.writeHead(404,headers);
+            res.write(JSON.stringify({
+                "status": "false",
+                "message": "無此 ID",
+                }));
+            res.end();
+        }
+    } else if (req.url.startsWith('/todos/') && req.method == 'PATCH') {
+        req.on('end', () => {
+        try {
+            const todo = JSON.parse(body).title;
+            const id = req.url.split('/').pop();
+            const index = todos.findIndex(todo => todo.id === id);
+            console.log(index);
+            if (index !== undefined && index !== -1) {
+                todos[index].title = todo;
+                res.writeHead(200,headers);
+                res.write(JSON.stringify({
+                    "status": "success",
+                    "data": todos,
+                    "message": "更新成功",
+                    }));
+                    res.end();
+            } else {
+                res.writeHead(404,headers);
+                res.write(JSON.stringify({
+                    "status": "false",
+                    "message": "無此 ID",
+                    }));
+                res.end();
+            }
+        } catch (error) {
+            console.log(error);
+            errorHandle(res);
+        }
+        });
+    } else if (req.method == 'OPTIONS') {
+        res.writeHead(200,headers);
+        res.end();
+    } else {
+    res.writeHead(404,headers);
+    res.write(JSON.stringify({
+        "status": "false",
+        "message": "無此網站路由",
+        }));
+    res.end();
+    } 
+}
+const server = http.createServer(requestListener);
+server.listen(3005, () => {
+    console.log('Server is running on port 3005');
+});
