a1f52fb6853ed8d5f36afa76335b31321efd7fe0
Holly05590 <ilexholly@gmail.com>
2026-06-19 06:22:54 +0800

    調整環境變數

--- server.js
+++ server.js
@@ -115,6 +115,6 @@ const requestListener = function (req, res) {
     } 
 }
 const server = http.createServer(requestListener);
-server.listen(3005, () => {
+server.listen(process.env.PORT || 3005, () => {
     console.log('Server is running on port 3005');
 });
